9445988edcba125f7911998405e058a178c84995
Robert McCutchen <rmccutchen2009@gmail.com>
2019-07-16 18:48:12 -0500

    Javascript: To-do List

---
+++ July/July 16th/Random Image/good-ads/app.js
@@ -0,0 +1,7 @@
+
+// call the getRandomAd function
+let randomImage = getRandomAd()
+// get the adImage by id
+let image = document.getElementById("adImage")
+// set the src property to the new random ad image  
+    image.src = randomImage
---
+++ July/July 16th/To-do List/javascript.js
@@ -0,0 +1,45 @@
+let addButton = document.getElementById("addButton")
+let taskTextBox = document.getElementById('taskTextBox')
+let pendingTasks = document.getElementById('pendingTasks')
+let completedTasks = document.getElementById('completedTasks')
+
+function clearTaskBar() {
+    taskTextBox.value= ""
+}
+
+addButton.addEventListener('click',function() {
+    let LiElement = document.createElement('li')
+    
+    let node = document.createElement("span")
+    node.innerHTML = taskTextBox.value
+    node.className = "label"
+    clearTaskBar()
+
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkBox")
+    checkbox.className = "checkBoxes"
+    
+
+    let delButton = document.createElement("button")
+    delButton.innerHTML = "Remove"
+
+    LiElement.append(checkbox)
+    LiElement.append(node)
+    LiElement.append(delButton)
+    pendingTasks.append(LiElement)
+
+    checkbox.addEventListener('click', function() {
+        if(this.checked){
+            completedTasks.append(LiElement)
+        } else {
+            pendingTasks.append(LiElement)
+        }
+    })
+    delButton.addEventListener('click', function() {
+        if(completedTasks.contains(LiElement)) {
+            completedTasks.removeChild(LiElement)
+        } else if(pendingTasks.contains(LiElement)) {
+            pendingTasks.removeChild(LiElement)
+        }  
+    })
+})
