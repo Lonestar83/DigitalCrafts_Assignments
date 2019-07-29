6ccf522e6fd0a129b0d616357f90e9fff3a1c8c4
Robert McCutchen <rmccutchen2009@gmail.com>
2019-07-29 13:17:34 -0500

    login page

--- July/July 28th/auth.js
+++ July/July 28th/auth.js
@@ -1,53 +1,14 @@
-//Get Elements
-const txtEmail = document.getElementById('txtEmail');
-const txtPassword = document.getElementById('txtPassword');
-const btnLogin = document.getElementById('btnLogin');
-const btnSignUp = document.getElementById('btnSignUp');
-const btnLogout = document.getElementById('btnLogout');
-const errorMessage = document.getElementById('errorMessage')
-
-
-function clearTxtBoxes() {
-    txtEmail.value = ""
-    txtPassword = ""
-}
-
-//Add login event
-btnLogin.addEventListener('click', x => {
-    //Get email and password
-    const email = txtEmail.value;
-    const password = txtPassword.value;
-    clearTxtBoxes();
-    const auth = firebase.auth();
-    //Sign in
-    const promise = auth.signInWithEmailAndPassword(email, password);
-    promise.catch(error => console.log(error.message));
-    window.location.href = "welcomepage.html";
-})
-
-//Add signup event
-btnSignUp.addEventListener('click', x => {
-    //Get email and password
-    const email = txtEmail.value;
-    const password = txtPassword.value;
-    clearTxtBoxes();
-    const auth = firebase.auth();
-    //Sign in
-    const promise = auth.createUserWithEmailAndPassword(email, password);
-    promise.catch(error => console.log(error.message));
-})
-
-btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-    window.location.href = "index.html";
-})
-
-//Add a realtime listener
-firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser) {
-        console.log(firebaseUser);
-        
-    } else {
-        console.log('not logged in')
-    }
-});
+let database = null;
+(function() {
+    var firebaseConfig = {
+    apiKey: "AIzaSyDN8XKIbmHBoS6b7MvOYDH3bXgf9u9O7zs",
+    authDomain: "auth-practice-4bf18.firebaseapp.com",
+    databaseURL: "https://auth-practice-4bf18.firebaseio.com",
+    projectId: "auth-practice-4bf18",
+    storageBucket: "auth-practice-4bf18.appspot.com",
+    messagingSenderId: "203847427175",
+    appId: "1:203847427175:web:93d335b461b64d84"
+    };
+    firebase.initializeApp(firebaseConfig);
+    database = firebase.database() 
+}());
