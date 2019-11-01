af7d015327e7b860802e4344136e0ccc7bf96cc9
Robert McCutchen <rmccutchen2009@gmail.com>
2019-10-31 21:38:53 -0500

    More Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -106,3 +106,15 @@ function fib(n) {
 
     return fib(n - 1) + fib(n - 2);
 }
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - i -1); j++) {
+            if (arr[j] > arr[j+1]) {
+                const lesser = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+}
