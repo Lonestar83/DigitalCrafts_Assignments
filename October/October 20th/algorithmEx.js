de5b20c311f412c3d9516b3d5d39a649dedc7b6b
Robert McCutchen <rmccutchen2009@gmail.com>
2019-11-08 20:23:16 -0600

    More Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -165,3 +165,25 @@ function reverseArray(arr) {
         return newArr.concat(reverseArray(arr.slice(1)), arr[0]);
     }
 }
+
+let caesarCipher = (str, key) => {
+    return str.toUpperCase().replace(/[A-Z]/g, c => String.fromCharCode((c.charCodeAt(0)-65 + key ) % 26 + 65));
+}
+
+var caesarShift = function(str, amount) {
+	if (amount < 0)
+		return caesarShift(str, amount + 26);
+	var output = '';
+	for (var i = 0; i < str.length; i ++) {
+		var c = str[i];
+		if (c.match(/[a-z]/i)) {
+			var code = str.charCodeAt(i);
+			if ((code >= 65) && (code <= 90))
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+		}
+		output += c;
+	}
+	return output;
+};
