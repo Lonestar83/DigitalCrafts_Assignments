f4f4b5a49b490f45d1e6b632c2a9de6f3d4f34b1
Robert McCutchen <rmccutchen2009@gmail.com>
2019-11-14 23:20:08 -0600

    More Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -233,3 +233,16 @@ function validate(phone) {
     const regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     return regex.test(phone)
 }
+
+function romanNumeral(num) {
+    var result = '';
+    var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    for (var i = 0; i <= decimal.length; i++) {
+        while (num%decimal[i] < num) {
+            result += roman[i];
+            num -= decimal[i];
+        }
+    }
+    return result;
+}
