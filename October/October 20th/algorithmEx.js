56fc4e83e4638dd10fbb7aa4ac6ea43a1192fe4a
Robert McCutchen <rmccutchen2009@gmail.com>
2019-11-19 23:38:35 -0600

    More Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -256,3 +256,21 @@ function arrayOfMultipes(num, length) {
 }
 
 let a = Maath.pow
+
+function permut(string) {
+    if (string.length < 2) return string;
+    
+    var permutations = [];
+    
+    for (var i=0; i < string.length; i++) {
+        var char = string[i];
+
+        if (string.indexOf(char) != i) continue;
+
+        var remainingString = string.slice(0,i) + string.slice(i+1, string.length);
+
+        for (var subPermutation of permut(remainingString))
+        permutations.push(char + subPermutation)
+    }
+    return permutations;
+}
