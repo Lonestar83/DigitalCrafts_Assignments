2353f90e10dba6e31961d47e163cafcc0a3ec5e6
Robert McCutchen <rmccutchen2009@gmail.com>
2019-11-23 22:48:18 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -408,3 +408,25 @@ document.write('<br> After sorting : ');
 
 // Print the sorted linked list 
 l.iterate() 
+
+function bubble_Sort(a)
+{
+    var swapp;
+    var n = a.length-1;
+    var x=a;
+    do {
+        swapp = false;
+        for (var i=0; i < n; i++)
+        {
+            if (x[i] < x[i+1])
+            {
+               var temp = x[i];
+               x[i] = x[i+1];
+               x[i+1] = temp;
+               swapp = true;
+            }
+        }
+        n--;
+    } while (swapp);
+ return x; 
+}
