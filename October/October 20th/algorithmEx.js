1a29182fb731d89613e4603a758b7c8c4e426e3a
Robert McCutchen <rmccutchen2009@gmail.com>
2019-11-28 23:35:18 -0600

    More Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -454,3 +454,17 @@ function toArray(obj) {
     }
     return arr;
 }
+
+var x = document.getElementById("demo");
+function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Gelocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML - "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
