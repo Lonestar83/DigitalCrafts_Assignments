cd401ee51fcf636246e5e53f62a0f0a47fe25936
Robert McCutchen <rmccutchen2009@gmail.com>
2019-11-29 22:37:17 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -465,6 +465,19 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    x.innerHTML - "Latitude: " + position.coords.latitude + 
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude; 
+}
+
+const twoSum = (arr, target) => {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                result.push(i);
+                result.push(j);
+            }
+        }
+    }
+    return result;
 }
