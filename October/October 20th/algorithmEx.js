188cf18cf39c1abe969ff4b6dfead99a704181ea
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-02 23:17:52 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -496,3 +496,15 @@ function draw() {
         ctx.stroke();
     }
 }
+
+function draq() {
+    var canvas = document.getElementById("canvas");
+    if(canvas.getContect) {
+        var context = canvas.getContext("2d");
+
+        context.fillStyle = "rgba(256,0,0)";
+        context.fillRect(15,10,55,50);
+        context.fillStyle = "rgba(0,0,200,0.6)";
+        context.fillRect(35, 30, 55, 50);
+    }
+}
