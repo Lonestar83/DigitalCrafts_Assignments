bd82c846c937ab05c71da55207bab14f52986b5a
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-04 23:13:15 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -508,3 +508,14 @@ function draq() {
         context.fillRect(35, 30, 55, 50);
     }
 }
+
+function* idMaker() {
+    var index = 0;
+    while(true)
+        yield index++;
+}
+
+var gen = idMaker();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
