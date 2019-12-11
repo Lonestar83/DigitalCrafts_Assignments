58be2e46013565c7d02fb9a4e257de615790e8b4
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-10 23:44:10 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -532,3 +532,22 @@ function filterNegatives(arr) {
 function squareDigits(num) {
     return Number(('' + num).split('').map(function(val) {return val * val;}).join(''));
 }
+
+function alphabetPosition(text) {
+    var newString = '';
+    var obj = 
+        {
+            a: 1, b: 2, c: 3, d: 4, 
+            e: 5, f: 6, g: 7, h: 8,
+            i: 9, j: 10, k: 11, l: 12, 
+            m: 13, n: 14, o: 15, p: 16, 
+            q: 17, r: 18, s: 19, t: 20,
+            u: 21, v: 22, w: 23, x: 24, 
+            y: 25, z: 26
+        };
+    var arr = text.replace(' ', '').split();
+    for(let i = 0; i < arr.length; i++) {
+        newString += obj[arr[i]];
+    }
+    return newString;
+}
