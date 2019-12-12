958db387596ff860f3f0367481a69214abe0ebc6
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-11 23:04:32 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -551,3 +551,22 @@ function alphabetPosition(text) {
     }
     return newString;
 }
+
+function findOdd(arr) {
+    var a = [], b = [], prev;
+    arr.sort();
+    for ( var i = 0; i < arr.length; i++) {
+        if ( arr[i] !== prev ) {
+            a.push(arr[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+          prev = arr[i];
+        }
+        for(let j in b) {
+            if(b[j] % 2 != 0) {
+                return a[j];
+            }
+        }
+}
