2f0e926627cb7fe9dd28f1a88a42ee68d439d70c
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-14 22:55:50 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -579,3 +579,13 @@ function findMissingLetter(array) {
         }
     }
 }
+
+function duplicateEncode(word){
+    return word
+        .toLowerCase()
+        .split('')
+        .map(function(a, i, w) {
+            return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+        })
+        .join('');
+}
