327e040788fe04e1c872a9655676d72c85ab068f
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-16 23:19:57 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -602,3 +602,16 @@ function order(words) {
     }
     return newArr.join(' ');
 }
+
+function isPrime(num) {
+    var isPrime;
+    if(num < 2) {
+        return isPrime = false;
+    }
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(num %1 == 0) {
+            return isPrime = false;
+        }
+    }
+    return isPrime = true;
+}
