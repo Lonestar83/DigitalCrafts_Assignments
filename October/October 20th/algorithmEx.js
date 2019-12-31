a776742b6c3795845f8399b3553851b16ce51b1d
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-30 22:45:44 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -683,3 +683,13 @@ function generateHashtag (str) {
         return '#' + str;
     }
 }
+
+var moveZeros = function (arr) {
+    for(let i = arr.length; i >= 0; i--) {
+        if(arr[i] === 0) {
+            arr.splice(i,1);
+            arr.push(0);
+        }
+    }
+    return arr;
+}
