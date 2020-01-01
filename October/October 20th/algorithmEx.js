ba0a93bd0a76ffe6d0f7cc8da7bcd63e6d16c3bd
Robert McCutchen <rmccutchen2009@gmail.com>
2019-12-31 23:05:12 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -693,3 +693,13 @@ var moveZeros = function (arr) {
     }
     return arr;
 }
+
+function validParentheses(parens) {
+    var n = 0;
+    for(let i = 0; i < parens.length; i++) {
+        if(parens[i] == '(') n++;
+        if(parens[i] == ')') n--;
+        if(n < 0) return false;
+    }
+    return n == 0;
+}
