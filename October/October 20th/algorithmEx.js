092b5fc3147d726746b99b0f47e339479b1e60cf
Robert McCutchen <rmccutchen2009@gmail.com>
2020-01-05 22:22:43 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -770,3 +770,11 @@ function getAllPermutations(string) {
     }
     return results;
 }
+
+function isSolved(board) {
+    board = board.join('-').replace(/,/g, '');
+    if(/222|2...2...2|2....2....2|2..2..2/.test(board)) return 2;
+    if(/111|1...1...1|1....1....1|1..1..1/.test(board)) return 1;
+    if(/0/.test(board)) return -1;
+    return 0;
+}
