6f3901f37be2b6abac94fc795524a586d24c6b6c
Robert McCutchen <rmccutchen2009@gmail.com>
2020-01-18 23:31:05 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -803,3 +803,13 @@ function sum(a, b) {
     }
     return (Number(a) + Number(b)).toString();
 }
+
+function rgb(r, g, b){
+    return toHex(r) + toHex(g) +toHex(b);
+}
+
+function toHex(d) {
+    if(d < 0) {return "00";}
+    if(d > 255) {return "FF";}
+    return ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
