a4c6474e1ce8891448549ea5cd195ae6c74b1198
Robert McCutchen <rmccutchen2009@gmail.com>
2020-01-23 23:27:18 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -834,3 +834,15 @@ function LCS(x, y) {
     }
     return arr.join("");
 }
+
+function listSquared(m, n) {
+    var arr = [];
+    for (var i = m; i <= n; i++){
+      var temp = 0;
+      for (var j = 1; j <= i; j++) {
+        if ( i % j == 0) temp += j*j;  
+      };
+      if ( Math.sqrt(temp) % 1 == 0) arr.push([i, temp]);
+    };
+    return arr;
+  }
