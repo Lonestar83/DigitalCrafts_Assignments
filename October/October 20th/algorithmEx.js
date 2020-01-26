aa7dd67b845078fa1cf3bb79c136d9bec58382b8
Robert McCutchen <rmccutchen2009@gmail.com>
2020-01-25 23:27:39 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -856,3 +856,17 @@ function listSquared(m, n) {
           }).join('');
       })
   }
+
+  function isMerge(s, part1, part2) {
+      if(s === "" && part1 === "" && par2 === "") return true;
+
+      if(s !== "") {
+          if(s[0] === part1[0] && part1[0] === part2[0]) {
+              return (isMerge(s.slice(1), part1.slice(1), part2) || isMerge(s.slice(1), part1, part2.slice(1)));
+          } else if (s[0] === part1[0]) {
+              return isMerge(s.slice(1), part1.slice(1), part2);
+          } else if (s[0] === part2[0]) {
+              return isMerge(s.slice(1), part1, part2.slice(1));
+          }
+      }
+  }
