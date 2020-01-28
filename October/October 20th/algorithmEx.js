6138664f9a29132dc73b768a609a96bbb8634822
Robert McCutchen <rmccutchen2009@gmail.com>
2020-01-27 22:04:52 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -874,3 +874,35 @@ function listSquared(m, n) {
   function toUnderscore(string) {
       return (''+string).replace(/[A-Z]/g, (c,i) => i ==0 ? c.toLowerCase() : '_'+c.toLowerCase());
   }
+
+  var whoEatsWho = function(zoo) {
+      const relation = {
+          antelope: ['grass'],
+          'big-fish': ['little-fish'],
+          bug: ['leaves'],
+          bear: ['big-fish', 'bug', 'chicken', 'cow', 'leaves', 'sheep'],
+          chicken: ['bug'],
+          cow: ['grass'],
+          fox: ['chicken', 'sheep'],
+          giraffe: ['leaves'],
+          lion: ['antelope', 'cow'],
+          panda: ['leaves'],
+          sheep: ['grass']
+      }
+
+      let res = [zoo]
+      zoo = zoo.split(',')
+
+      let isRelation = (x,y) => relation[x] && relation[x].includes(y)
+      let checkAndRemove = (zoo, i, j) => {
+          if(isRelation(zoo[i], zoo[j])) {
+              res.push(zoo[i] + ' eats ' + zoo[j])
+              return zoo.splice(j,1)
+          }
+          return false;
+      }
+
+      while (zoo.some((x, i) => checkAndRemove(zoo, i, i-1) || checkAndRemove(zoo, i, i+1)));
+      res.push(zoo + '')
+      return res;
+  }
