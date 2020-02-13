94f3240d3c7ede5ce195a1d01645c74e85584e8e
Robert McCutchen <rmccutchen2009@gmail.com>
2020-02-12 20:11:04 -0600

    Algorithm Exercises

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -1035,6 +1035,13 @@ const flatten = (...arr) => {
             i++;
         }
     }
-    
+
     return arr;
+}
+
+const cardsMap = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, J: 10, Q: 10, K: 10, A: 1}
+
+const scoreHand = cards => {
+    const sum = cards.reduce((acc, card) => acc + cardsMap[card], 0);
+    return sum < 12 && cards.includes('A') ? sum + 10 : sum;
 }
