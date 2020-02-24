0d9553f403205d4409d48923a2d1515ab267e144
Robert McCutchen <rmccutchen2009@gmail.com>
2020-02-23 19:39:47 -0600

    Algorithm Exercise

--- October/October 20th/algorithmEx.js
+++ October/October 20th/algorithmEx.js
@@ -1079,3 +1079,10 @@ function findOccurence(X, Y, V) {
     }
     return counter;
 }
+
+function isPP(n) {
+    for (var m =2; m * m <= n; m++)
+        for (var k =2; Math.pow(m, k) <= n; k++)
+            if (Math.pow(m,k) == n) return [m,k];
+    return null;
+}
